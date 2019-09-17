340a358333e6a21a8515c9701f0d92ef6a28f912
nelson pino <software@etiquetas.com.do>
2019-09-17 11:13:53 -0400

    Incluir el Archivo R de Recursos

--- PackLibrary/Main.cs
+++ PackLibrary/Main.cs
@@ -7,17 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data;
 
 namespace PackLibrary
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
         }
-
-        private void Form1_Load(object sender, EventArgs e)
+        string CONNNECTION_STRING = @"Server=SOPORTE\ETIQUETAS;Catalog Initial=BDBioAdminSQL;Integrated  ";
+        private void Main_Load(object sender, EventArgs e)
         {
 
         }
---
+++ PackLibrary/R.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackLibrary
+{
+    public class R
+    {
+        public class SQL_SERVER
+        {
+            public static string SERVIDOR = @"SOPORTE\ETIQUETAS";
+        }    
+        public class DATABASE
+        {
+            public static string NAME_BIOADMIN = "BDBioAdminSQL";
+            public static string USER = "soporte";
+            public static string PASSWORD = "Jossycar5%";
+        }
+    }
+}
